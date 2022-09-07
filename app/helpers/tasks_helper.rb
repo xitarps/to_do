ebdbb4ab537870646f12b3eeca6c7ab783d7fad6
Rodrigo - Xita rps <xitarps@gmail.com>
2022-09-07 17:03:28 -0300

    Style, add helper and refactor of Tasks

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,2 +1,11 @@
 module TasksHelper
+  def symbol(task)
+    raise 'TasksHelper symbol method - variable "task" is not a task' unless task.is_a? Task
+
+    case task.current_status
+    when :pending then OpenStruct.new(icon: '»', color: :primary)
+    when :done    then OpenStruct.new(icon: '✓', color: :success)
+    when :expired then OpenStruct.new(icon: '⊗', color: :danger )
+    end
+  end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,16 @@
 class Task < ApplicationRecord
   validates :description, presence: true
   validates :done, inclusion: { in: [true, false] }
+
+  def current_status
+    status
+  end
+
+  private
+
+  def status
+    return :done    if done?
+    return :expired if due_date.past?
+    :pending
+  end
 end
