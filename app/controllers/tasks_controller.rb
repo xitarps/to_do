ec0dea8c691ee127b6c4a99935bd5b3b5f510a37
Rodrigo - Xita rps <xitarps@gmail.com>
2022-09-08 01:06:54 -0300

    Add sub tasks features

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -41,6 +41,6 @@ class TasksController < ApplicationController
   end
 
   def task_params
-    params.require(:task).permit(:description, :due_date, :done)
+    params.require(:task).permit(:description, :due_date, :done, :parent_id)
   end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -2,6 +2,20 @@ class Task < ApplicationRecord
   validates :description, presence: true
   validates :done, inclusion: { in: [true, false] }
 
+  belongs_to :parent,    class_name: 'Task', optional: true
+  has_many   :sub_tasks, class_name: 'Task',
+             foreign_key: :parent_id, dependent: :destroy
+
+  scope :only_parents, -> { where(parent_id: nil) }
+
+  def is_parent?
+    !!parent.nil?
+  end
+
+  def is_subtask?
+    !is_parent?
+  end
+
   def current_status
     status
   end
